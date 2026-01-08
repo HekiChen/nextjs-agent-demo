80f2235bada200d74956d932a83bbaf1b2c2a43f
Heki, Chen <hchen112@its.jnj.com>
2026-01-08 11:56:02 +0800

    Reset

--- prisma/migrations/initial_migration/migration.sql
+++
@@ -1,6 +0,0 @@
--- Initial migration for User model
-CREATE TABLE "User" (
-  "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR(255),
-  "email" VARCHAR(255) UNIQUE
-);
--- src/components/ExampleComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ExampleComponent: React.FC = () => {
-  return <div>Hello, Next.js!</div>;
-};
-
-export default ExampleComponent;
